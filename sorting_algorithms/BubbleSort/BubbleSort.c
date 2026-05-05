3586622ca2f7cf31fe182e9228473400aa12c808
Yuxiang Wang <yw149@illinois.edu>
2026-05-05 19:26:13 +0800

    add another solution for bubblesort

--- sorting_algorithms/BubbleSort/BubbleSort.c
+++ sorting_algorithms/BubbleSort/BubbleSort.c
@@ -11,7 +11,24 @@ int32_t BubbleSort(int32_t arr[], int32_t n){
     }
     n--;
     return BubbleSort(arr, n);
-}   
+}  
+
+void BubbleSort2(int32_t arr[], int32_t n)
+{
+    int32_t flag;
+    do
+    {
+        flag = 0;
+        for(int32_t i = 0; i < n; i++)
+        {
+            if(arr[i] > arr[i+1])
+            {
+                swap(&arr[i], &arr[i+1]);
+                flag = 1;
+            }
+        }
+    }while(flag);
+}
 
 void swap(int32_t *x, int32_t *y){
     int32_t temp;
--- sorting_algorithms/BubbleSort/BubbleSort.h
+++ sorting_algorithms/BubbleSort/BubbleSort.h
@@ -4,6 +4,7 @@
 #include<stdint.h>
 
 int32_t BubbleSort(int32_t arr[], int32_t n);
+int32_t BubbleSort2(int32_t arr[], int32_t n);
 void swap(int32_t *x, int32_t *y);
 
 #endif
