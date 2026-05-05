e311d033c46c2df1676b93f94678887f4ace2621
Yuxiang Wang <yw149@illinois.edu>
2026-05-05 19:13:02 +0800

    finish 08_Append.c

--- linkedlist/stanford_18_problems/07_InsertSort.c
+++ linkedlist/stanford_18_problems/07_InsertSort.c
@@ -2,25 +2,14 @@
  Given a list, rearranges its nodes so they are sorted in
  increasing order. It should use SortedInsert().
 */
-
 #include "../Basic_Operations/BasicOperations.h"
 #include <stdlib.h>
 
 void InsertSort(struct node** headRef)
 {
-    struct node* current = (*headRef)->next;
-    struct node* traverse;
-    struct node* prev;
+    struct node* current = *headRef;
     while(current->next != NULL)
     {
-        traverse = (*headRef);
-        while(traverse->data < current->data)
-        {
-            traverse = traverse->next;
-            prev = traverse;
-        }
-        prev->next = current;
-        current->next = traverse;
-        current = current->next;
+        
     }
 }
---
+++ linkedlist/stanford_18_problems/08_Append.c
@@ -0,0 +1,18 @@
+// Append 'b' onto the end of 'a', and then set 'b' to NULL.
+#include "../Basic_Operations/BasicOperations.h"
+#include <stdlib.h>
+
+void Append(struct node** aRef, struct node** bRef)
+{
+    if ((*aRef) == NULL) {(*aRef) = (*bRef);}
+    else
+    {
+        struct node* current = *aRef;
+        while(current->next != NULL)
+        {
+            current = current->next;
+        }
+        current = *bRef;
+    }
+    (*bRef) = NULL;
+}
