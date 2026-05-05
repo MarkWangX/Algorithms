6926923e8a98cf507f00c33154fd4c7036a91087
Yuxiang Wang <yw149@illinois.edu>
2026-05-05 19:12:27 +0800

    finish binary search

---
+++ Searching_algorithms/BinarySearch.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+
+int32_t BinarySearch(int32_t arr[], int32_t target, int32_t low, int32_t high)
+{
+    if (low > high) 
+    {
+        return -1;
+    }
+    int32_t middle = low + (high - low) / 2;
+    if (arr[middle] < target) { 
+        return BinarySearch(arr, target, middle + 1, high); 
+    }
+    else if (arr[middle] > target) { 
+        return BinarySearch(arr, target, low, middle - 1); 
+    }
+    else {
+        return middle; 
+    }
+}
+
+int32_t BinarySearch(int32_t arr[], int32_t target, int32_t low, int32_t high)
+{
+    int32_t middle;
+    while(high>=low)
+    {
+        middle = (high + low) / 2;
+        if (arr[middle] == target) { 
+            return middle; 
+        }
+        else if (arr[middle] < target) { 
+            low = middle + 1;
+        }
+        else {
+            high = middle - 1; 
+        }        
+    }
+}
